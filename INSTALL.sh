666052ce486b6089bb9914d3ed48dd183dfa21d8
everest <liamwaldron7009@protonmail.com>
2022-04-25 14:55:09 +0000

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+echo "[ i ] Getting executable..."
 wget https://raw.githubusercontent.com/everest-linux/xfetch/main/xfetch.py
+echo "[ i ] Installing executable..."
 cp xfetch.py xfetch
 chmod +x xfetch
 mv xfetch /usr/bin
+echo "[ i ] xfetch installed successfully."
